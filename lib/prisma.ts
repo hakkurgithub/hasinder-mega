44130282710379acb6460ff611467e524ed51d71
yararhukuk <av.furkanyrr@gmail.com>
2026-03-14 22:42:31 +0300

    Fix: downgrade React 18 for stability

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,16 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['error'],
-})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export const disconnectPrisma = async () => {
-  if (process.env.NODE_ENV === 'production') {
-    await prisma.$disconnect()
-  }
-}
-
-export default prisma
