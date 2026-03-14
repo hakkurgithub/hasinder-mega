0035a225d1ec0f265b65a29987dd8a1698d5b95b
yararhukuk <av.furkanyrr@gmail.com>
2026-03-14 22:44:56 +0300

    Fix: add missing disconnectPrisma export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,16 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['error'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export const disconnectPrisma = async () => {
+  if (process.env.NODE_ENV === 'production') {
+    await prisma.$disconnect()
+  }
+}
+
+export default prisma
